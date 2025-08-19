285ed77aefeb89df92aeea465864ec3f322dfcb5
Dhyani1217 <dhyani1217patel@gmail.com>
2025-08-19 12:40:22 +0530

    Add files via upload

---
+++ D1.C
@@ -0,0 +1,9 @@
+#include<stdio.h>     // header file
+  void main()           // function
+  {
+      int a,b,c;
+      printf("Enter 2 NOS. ");
+      scanf("%d%d",&a,&b);    // input
+      c = a + b;
+      printf("%d+%d= %d\n",a,b,c);
+  }
