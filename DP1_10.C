9c61ce6bff668d0b6c22aa6dc9cb6d6ba8574e5f
Dhyani1217 <dhyani1217patel@gmail.com>
2025-08-26 12:46:38 +0530

    Add files via upload

---
+++ DP1_10.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+float a,b,c;
+clrscr();
+printf("enter money in DOLLAR: ",a);
+scanf("%f",&a);
+b=a*48;
+c=b/70;
+printf("money in pound is %f",c);
+getch();
+}
