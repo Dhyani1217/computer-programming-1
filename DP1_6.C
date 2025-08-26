aeb6a445131365a419b07973141ff13604b907c0
Dhyani1217 <dhyani1217patel@gmail.com>
2025-08-26 12:22:59 +0530

    Add files via upload

---
+++ DP1_6.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("enter time in hours:  ");
+scanf("%d",&a);
+b=a*60;
+printf("time in minutes is %d",b);
+getch();
+}
---
+++ DP1_7.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("enter time in minutes:  ");
+scanf("%d",&a);
+b=a/60;
+printf("time in hours is %d",b);
+getch();
+}
